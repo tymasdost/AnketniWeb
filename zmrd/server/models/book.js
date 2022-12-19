05d15e157539f214f409864e25c2a0fac18bd0b3
Jaroslav Nový <92918228+tymasdost@users.noreply.github.com>
2022-12-19 21:06:28 +0100

    Delete zmrd directory

--- zmrd/server/models/book.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const bookSchema = mongoose.Schema({
-  name: { type: String, required: true },
-  year: { type: Number, required: true },
-});
-
-module.exports = mongoose.model("Book", bookSchema);
--- zmrd/server/routes/account.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const accountController = require("../controllers/account");
-
-router.get("/", accountController.getAccounts);
-router.get("/:email", accountController.getAccount);
-
-module.exports = router;
